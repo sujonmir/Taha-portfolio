3bfa2c9f67e98e130b4c97d856481406f8d4387c
Sujon Mir <sujonmhk786@gmail.com>
2023-03-25 09:29:54 +0600

    2nd commit

--- js/activeLink.custom.js
+++ js/activeLink.custom.js
@@ -1,7 +1,7 @@
 // active menu
 
 $(window).scroll(function(){
-  if ($(this).scrollTop()>5000) {
+  if ($(this).scrollTop()>3200) {
     $("#li_7").addClass("active_link");
     $("#li_6").removeClass("active_link");
     $("#li_5").removeClass("active_link");
@@ -9,23 +9,7 @@ $(window).scroll(function(){
     $("#li_3").removeClass("active_link");
     $("#li_2").removeClass("active_link");
     $("#li_1").removeClass("active_link");
-  }else if ($(this).scrollTop()>4150) {
-    $("#li_7").removeClass("active_link");
-    $("#li_6").addClass("active_link");
-    $("#li_5").removeClass("active_link");
-    $("#li_4").removeClass("active_link");
-    $("#li_3").removeClass("active_link");
-    $("#li_2").removeClass("active_link");
-    $("#li_1").removeClass("active_link");
-  }else if ($(this).scrollTop()>3650) {
-    $("#li_7").removeClass("active_link");
-    $("#li_6").removeClass("active_link");
-    $("#li_5").addClass("active_link");
-    $("#li_4").removeClass("active_link");
-    $("#li_3").removeClass("active_link");
-    $("#li_2").removeClass("active_link");
-    $("#li_1").removeClass("active_link");
-  }else if ($(this).scrollTop()>2250) {
+  }else if ($(this).scrollTop()>2010) {
     $("#li_7").removeClass("active_link");
     $("#li_6").removeClass("active_link");
     $("#li_5").removeClass("active_link");
@@ -33,7 +17,7 @@ $(window).scroll(function(){
     $("#li_3").removeClass("active_link");
     $("#li_2").removeClass("active_link");
     $("#li_1").removeClass("active_link");
-  }else if ($(this).scrollTop()>1500) {
+  }else if ($(this).scrollTop()>1190) {
     $("#li_7").removeClass("active_link");
     $("#li_6").removeClass("active_link");
     $("#li_5").removeClass("active_link");
@@ -41,7 +25,7 @@ $(window).scroll(function(){
     $("#li_3").addClass("active_link");
     $("#li_2").removeClass("active_link");
     $("#li_1").removeClass("active_link");
-  }else if ($(this).scrollTop()>550) {
+  }else if ($(this).scrollTop()>630) {
     $("#li_7").removeClass("active_link");
     $("#li_6").removeClass("active_link");
     $("#li_5").removeClass("active_link");
