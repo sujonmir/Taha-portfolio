95e23572f580e32d985dc5f14441de2db8e95756
Sujon Mir <sujonmhk786@gmail.com>
2023-03-25 06:58:11 +0600

    1st commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Usage
+'use strict';
+
+// jQuery noConflict declaration
+jQuery.noConflict();
+// run jQuery
+jQuery(document).ready(function( $ ){
+
+    // jQuery Smooth Scroll Window
+    $(this).smoothScrollWindow();
+
+});
+
+
